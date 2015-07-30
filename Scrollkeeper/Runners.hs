6e7c6a85d6f286967edf5ad4f550ba60a613eec1
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-07-30 04:12:18 +0200

    Finish execution model and Runners

--- Scrollkeeper/Runners.hs
+++ Scrollkeeper/Runners.hs
@@ -3,7 +3,7 @@
 -- combinations of those. A signature of a runner is
 -- ``Parsec String a (Entry, EntryAdder) -> Parsec String ParseState ()``
 -- where @ParseState@, as defined in Types.hs, is
---``(Entry, EntryAdder, Space)``.
+-- ``(Entry, EntryAdder, Space)``.
 --
 -- Runners can be thought of performing the following instructions —
 -- "take a combination of user-supplied parsers and return a parser
@@ -35,9 +35,38 @@ liftpWith t x = do
   modifyState $ \(a, s) -> let f' = f e a
                            in (fst f', t (snd f') : s)
 
+-- | Evaluating is the last step in the chain of stateful transformations
+-- which form the execution flow of Scrollkeeper. This function should be
+-- called on the lifted aggregate of parsers. Implementation of this function
+-- is very simple and we include it in documentation for the sake of readers
+-- understanding the key part of how the accumulated data is taken out of
+-- the combined lifted parsers. Here it goes:
+--
+-- ```
+-- evalp = flip (>>) $ getState >>= return . conc
+-- ```
+--
+-- which, for innocent reader, is a perverted way to write
+--
+-- ```
+-- evalp x = x >> (getState >>= return . conc)
+-- ```
+--
+-- which, for those who prefer sugar, is the same as
+--
+-- ```
+-- evalp x = do
+--   x
+--   (entry, accumulator) <- getState
+--   return $ entry : accumulator
+-- ```
 evalp :: Parsec String ParseState () -> Parsec String ParseState Space
-evalp x = x >> getState >>= return . conc
+evalp = flip (>>) $ getState >>= return . conc
+
+-- | Runner for "evaluated" parsers.
+runpEvaluated :: Parsec String ParseState Space -> String -> PE Space
+runpEvaluated p x = runIdentity $ runParserT p (empty, []) "" x
 
 -- | Runner for "lifted" parsers.
-run :: Parsec String ParseState () -> String -> PE Space
-run p x = runIdentity $ runParserT (evalp p) (empty, []) "" x
+runpLifted :: Parsec String ParseState () -> String -> PE Space
+runpLifted p = runpEvaluated $ evalp p
--- Scrollkeeper/Types.hs
+++ Scrollkeeper/Types.hs
@@ -8,7 +8,7 @@ import           Data.Time.Clock     (NominalDiffTime)
 import           Data.Time.LocalTime (LocalTime)
 import           Text.Parsec
 
--- | Strict record for data bits, contained in dimensions of the space
+-- | Strict record for data contained in @Dimension@s of the @Space@
 data Tag = S !String                 -- ^ A single string
          | W ![String]               -- ^ List of words
          | F !Float                  -- ^ Float
@@ -25,8 +25,8 @@ type Dimension  = String
 -- as of a n-dimensional body in ∞-dimensional space.
 type Entry = Map Dimension Tag
 
--- | ∞-dimensional space containing n-dimensional bodies as @Entry@.
--- Modelled as a list.
+-- | Inhabited ∞-dimensional space containing n-dimensional bodies
+-- represented as @Entry@. Modelled as a list.
 type Space = [Entry]
 
 -- | Function type for functions that specify how a certain @Entry@
@@ -37,22 +37,23 @@ type Space = [Entry]
 -- This type signature might be read as follows:
 --
 -- An entry adder is a function that takes @Entry@ that is currently
--- stored in the registry (if any) and the newely parsed @Entry@ and returns
--- a tuple where the first element is either Just @Entry@ that has
--- to be added to the @Space@ or Nothing and second element is the
+-- stored in the registry (if any*) and the newely parsed @Entry@ and returns
+-- a tuple where the first element is either inhabited @Entry@ that has
+-- to be added to the @Space@ or empty @Entry@ and second element is the
 -- entry that has to be stored in the registry.
 --
 -- You can combine adders with (.>.) function from Scrollkeeper.Extras.
+--
+-- * — lack of @Entry@ in the registry is modelled with an empty Map.
 type EntryAdder = Entry -> Entry -> (Entry, Entry)
 
--- | State of parser. It's a three-registry machine storing an @Entry@
--- in the first registry and an @EntryAdder@ stored in the second registry.
--- accumulated @Space@ is held in the third registry and is returned after
--- top-to-bottom evaluation.
+-- | State of parser. It's a two-registry machine storing an @Entry@
+-- in the first registry while accumulated @Space@ is held in the second
+-- registry and is returned after top-to-bottom evaluation.
 type ParseState = (Entry, Space)
 
--- | Shorthand for return value of parsers
+-- | Shorthand for return value of parsers.
 type PE a = Either ParseError a
 
--- | Shorthand for NominalDiffTime
+-- | Shorthand for NominalDiffTime.
 type DTime = NominalDiffTime
